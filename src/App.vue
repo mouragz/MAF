3eec516dad95566376b9e8644bccecee8cca21de
mouragz <nourazoubi@gmail.com>
2021-02-20 23:10:55 +0200

    implement new menu design

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,13 @@
   <div id="app">
     <upperComponent @OpenSidebar="OpenSidebar" />
     <lowerComponent />
-    <sidebarComponent :sidebarOpened="isSidebarOpened">
+    <sidebarComponent @OpenSidebar="OpenSidebar" :sidebarOpened="isSidebarOpened">
       <ul class="sidebar-panel-nav">
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
     </sidebarComponent>
-    <div class="sidebar-backdrop" @click="OpenSidebar" v-if="isSidebarOpened"></div>
   </div>
 </template>
 <script>
