fdb1de529f1e2a003d5ef1a26f6850ea767f8383
mouragz <nourazoubi@gmail.com>
2021-02-20 23:48:08 +0200

    deploy

---
+++ precache-manifest.3aaf6b35dabfc1ec69b7b5b8519015df.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e6f66e138d933c6a587",
+    "url": "/maf/css/app.0ab76f22.css"
+  },
+  {
+    "revision": "6c63a4b5071bff132272b2168ee373cf",
+    "url": "/maf/fonts/Larsseit.6c63a4b5.woff2"
+  },
+  {
+    "revision": "72945abd6e419fc09b4ab40dacdea78e",
+    "url": "/maf/fonts/Larsseit.72945abd.woff"
+  },
+  {
+    "revision": "8f95f19e00e03b7afd899f2c7099b3d9",
+    "url": "/maf/fonts/Larsseit.8f95f19e.eot"
+  },
+  {
+    "revision": "d5bd523d9152e80458f764c62c5bb9df",
+    "url": "/maf/fonts/Larsseit.d5bd523d.ttf"
+  },
+  {
+    "revision": "2414093e10109bd9ac59738167c05eb5",
+    "url": "/maf/fonts/NoeDisplay-Bold.2414093e.woff2"
+  },
+  {
+    "revision": "30a71dcf9a1c39558ca4e97c92a4b2fa",
+    "url": "/maf/fonts/NoeDisplay-Bold.30a71dcf.ttf"
+  },
+  {
+    "revision": "5221ebefc013e462c0aabcab361b9e42",
+    "url": "/maf/fonts/NoeDisplay-Bold.5221ebef.eot"
+  },
+  {
+    "revision": "f0c4b9a479fab76f3fd5521e9ad08850",
+    "url": "/maf/fonts/NoeDisplay-Bold.f0c4b9a4.woff"
+  },
+  {
+    "revision": "84c7387055c2af8ce650c613f0a7fde7",
+    "url": "/maf/img/NT.84c73870.svg"
+  },
+  {
+    "revision": "44d26aade61781e12840a7519105d114",
+    "url": "/maf/img/arrow.44d26aad.svg"
+  },
+  {
+    "revision": "45a0dee7d58b5016a5a342b7e88e9e98",
+    "url": "/maf/img/bright.45a0dee7.svg"
+  },
+  {
+    "revision": "2aae9fc727e870ce8e5dc733f77e4d20",
+    "url": "/maf/img/desert.2aae9fc7.svg"
+  },
+  {
+    "revision": "422f65f2e46fcca7822497d7275e70fe",
+    "url": "/maf/img/dots.422f65f2.svg"
+  },
+  {
+    "revision": "c3eace36b9e9e69d04924d1f3bcd3081",
+    "url": "/maf/img/facebook.c3eace36.svg"
+  },
+  {
+    "revision": "9c90448049bef9f70d12e6827dd94077",
+    "url": "/maf/img/instagram.9c904480.svg"
+  },
+  {
+    "revision": "5cfaa91225e2474f451671fa0a28c68d",
+    "url": "/maf/img/main.5cfaa912.png"
+  },
+  {
+    "revision": "9cb67a893d2f82f3e4e3eaab5b894265",
+    "url": "/maf/img/rum.9cb67a89.png"
+  },
+  {
+    "revision": "8297cd8d2ffe4f0a6ce859fadf738e41",
+    "url": "/maf/img/search.8297cd8d.svg"
+  },
+  {
+    "revision": "d308220be49b2bce75448a396e7169ed",
+    "url": "/maf/img/twitter.d308220b.svg"
+  },
+  {
+    "revision": "00f7cc3fba21eb2123bda26d2346d5d3",
+    "url": "/maf/index.html"
+  },
+  {
+    "revision": "6e6f66e138d933c6a587",
+    "url": "/maf/js/app.a1d341dd.js"
+  },
+  {
+    "revision": "0d30d5fa5f76690633c8",
+    "url": "/maf/js/chunk-vendors.919c39de.js"
+  },
+  {
+    "revision": "ab7f5a6ea63c7bbe6af2c9dd95d79e88",
+    "url": "/maf/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/maf/robots.txt"
+  }
+]);
